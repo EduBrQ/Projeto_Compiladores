a929fe0a7544e0fc336d9f16efd63963cc5710d6
unknown <pabliciotjg@gmail.com>
2017-10-13 23:56:52 -0300

    Inclus~ao da funç~ao tokenizer para gerar a separaçao de tokens e lexemas depois de um codigo lido, incluidas algumas regras de produçao para a linguagem mini php e gerada uma tabela num modal que apresenta os tokens separadamente

--- assets/js/analise_lexica/analise_lexica.js
+++ assets/js/analise_lexica/analise_lexica.js
@@ -34,37 +34,40 @@ $(document).on('click', '#analisar', function () {
 
     $('#myTable').remove();
 
-    var table = '<table id="myTable" class="table table-responsive">'+
-                '<thead>'+
-                    '<tr>'+
-                        '<th>TOKEN</th>'+
-                        '<th>LEXEMA</th>'+
-                    '</tr>'+
-                '</thead>'+
-                '<tbody>'+
-                '</tbody>';
+    // CHAMA A FUNÇÃO DE GERAR TABELA DE TOKENS E LEXEMAS
+    var table = geraTabeta(arrayTokens);
 
-    $.each(arrayTokens, function (index, item) {
-
-        console.log(item);
-
-        table += '<tr>' +
-                    '<td>'+ item[0] +'</td>' +
-                    '<td>'+ item[1] +'</td>' +
-                '</tr>';
-
-    });
-
-    table += '</table>';
-
+    // ADICIONA A TABELA AO CONTENT
     $('#content').append(table);
 
     // CHAMA O MODAL COM A TABELA DE TOKENS E LEXEMAS
     $('.modal-title').text('Análise Léxica');
     $('#myModal').modal('show');
-    $('.addCavalo').show();
-
-
-
 });
 
+function geraTabeta(arrayTokens) {
+    var table = '<table id="myTable" class="table table-responsive">'+
+                    '<thead>'+
+                        '<tr>'+
+                            '<th>TOKEN</th>'+
+                            '<th>LEXEMA</th>'+
+                        '</tr>'+
+                    '</thead>'+
+                    '<tbody>'+
+                    '</tbody>';
+
+    $.each(arrayTokens, function (index, item) {
+
+        console.log(item);
+
+        table +=    '<tr>' +
+                        '<td>'+ item[0] +'</td>' +
+                        '<td>'+ item[1] +'</td>' +
+                    '</tr>';
+
+    });
+
+    table += '</table>';
+
+    return table;
+}
